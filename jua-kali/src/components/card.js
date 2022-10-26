ceaa7027d98faf33655187d63d5c6176c7b4525b
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-26 18:30:57 +0300

    basic formatting for card container

--- jua-kali/src/components/card.js
+++ jua-kali/src/components/card.js
@@ -6,11 +6,12 @@ function Card() {
       <span id="card_image">
         <h2 className="card_city">Nairobi</h2>
       </span>
+
       <span className="card">
-        <p className="card_temp">Temperature</p>
-        <p className="card_wind">Wind</p>
-        <p className="card_desc">Description</p>
-        <h2 className="card_fore">Forecast</h2>
+        <div className="card_temp">Temperature</div>
+        <div className="card_wind">Wind</div>
+        <div className="card_desc">Description</div>
+        <div className="card_fore">Forecast</div>
         <div className="card_buttons">
           <button>Day 1</button>
           <button>Day 2</button>
