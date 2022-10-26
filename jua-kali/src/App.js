c3969c821809eaa433b3320da954cdadb22cb1e3
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-26 14:49:06 +0300

    create serch bar

--- jua-kali/src/App.js
+++ jua-kali/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Card from "./components/card";
+import Search from "./components/search";
 
 function App() {
   return (
     <>
+      <Search />
       <Card />
     </>
   );
---
+++ jua-kali/src/components/search.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Search() {
+  return (
+    <form>
+      <input type="text" placeholder="Search City" />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
+
+export default Search;
